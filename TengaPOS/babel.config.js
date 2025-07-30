7d7c043bef1c680217dd54cb9c6b9d7520de3cee
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-30 10:48:11 +0200

    [DELETE]: WatermelonDB broken config

--- TengaPOS/babel.config.js
+++ TengaPOS/babel.config.js
@@ -1,11 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [
-    ["@babel/plugin-proposal-decorators", { "legacy": true }]
-  ];
+  let plugins = [];
 
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel', "module:metro-react-native-babel-preset"],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
 
     plugins,
   };
