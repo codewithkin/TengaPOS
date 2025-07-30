200206a35c15ec743617132197a6d5143d2cb88c
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-30 13:22:51 +0200

    [ADD]: Get all products route and controller

---
+++ tengapos-server/src/controllers/products/getAllProducts.ts
@@ -0,0 +1,39 @@
+import { Context } from "hono";
+import { prisma } from "../../helpers/prisma";
+
+export default async function getAllProducts(c: Context) {
+    try {
+        // Get the business's id
+        const id = c.req.query("id");
+
+        // Find the business with that id
+        const business = await prisma.business.findUnique({
+            where: {
+                id
+            }
+        });
+
+        // Throw an error if the business doesn't exist
+        if (!business) {
+            return c.json({
+                message: "This business doesn't exist"
+            }, 400);
+        }
+
+        // Otherwise....get the business' products
+        const products = await prisma.product.findMany({
+            where: {
+                businessId: id
+            }
+        });
+
+        // Return the products
+        return c.json(products);
+    } catch (e) {
+        console.log("An error occured while getting business's products: ", e);
+
+        return c.json({
+            message: "An error occured while getting your products: "
+        }, 500)
+    }
+}
--- tengapos-server/src/routes/index.ts
+++ tengapos-server/src/routes/index.ts
@@ -4,6 +4,11 @@ import VerifyEmail from "../controllers/auth/verify-email";
 import signIn from "../controllers/auth/signin";
 export const routes = new Hono();
 
+/* Auth routes */
 routes.post("/auth/signup", signUp);
 routes.post("/auth/signin", signIn);
 routes.get("/auth/verify-email", VerifyEmail);
+
+/* Product routes */
+// Get all products
+routes.get("/products", );
