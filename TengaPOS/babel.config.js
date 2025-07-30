61eabe9e30e44ff4cbae14a247e375b7d2dbaaf4
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-30 09:20:44 +0200

    [ADD]: RN chart and other deps

--- TengaPOS/babel.config.js
+++ TengaPOS/babel.config.js
@@ -1,9 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
+  let plugins = [
+    ["@babel/plugin-proposal-decorators", { "legacy": true }]
+  ];
 
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel', "module:metro-react-native-babel-preset"],
 
     plugins,
   };
