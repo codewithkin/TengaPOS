1ef456faf77cb4a1cf1f0777fd6be715ef05c76c
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-01 11:52:33 +0200

    [ADD]: Payment method to store

--- TengaPOS/stores/useSaleStore.ts
+++ TengaPOS/stores/useSaleStore.ts
@@ -4,6 +4,7 @@ type SaleData = {
     productIds: string[];
     customerId: string;
     businessId: string;
+    paymentMethod: string;
     setProductIds: (productIds: string[]) => void;
     addProductId: (productId: string) => void;
     removeProductId: (productId: string) => void;
@@ -16,8 +17,10 @@ export const useSaleStore = create<SaleData>()((set) => ({
     productIds: [],
     customerId: "",
     businessId: "",
+    paymentMethod: "",
 
     setProductIds: (productIds) => set({ productIds }),
+    setPaymentMethod: (paymentMethod: string) => set({ paymentMethod }),
     addProductId: (productId) =>
         set((state) => ({
             productIds: [...state.productIds, productId],
