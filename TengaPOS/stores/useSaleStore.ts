1498fe9cbc01c8f65352d4f87925952e8b528234
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-01 11:38:42 +0200

    [ADD]: Zustand store for salesData

---
+++ TengaPOS/stores/useSaleStore.ts
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+
+type SaleData = {
+    productIds: string[];
+    customerId: string;
+    businessId: string;
+    setProductIds: (productIds: string[]) => void;
+    addProductId: (productId: string) => void;
+    removeProductId: (productId: string) => void;
+    setCustomerId: (customerId: string) => void;
+    setBusinessId: (businessId: string) => void;
+    resetSale: () => void;
+};
+
+export const useSaleStore = create<SaleData>()((set) => ({
+    productIds: [],
+    customerId: "",
+    businessId: "",
+
+    setProductIds: (productIds) => set({ productIds }),
+    addProductId: (productId) =>
+        set((state) => ({
+            productIds: [...state.productIds, productId],
+        })),
+    removeProductId: (productId) =>
+        set((state) => ({
+            productIds: state.productIds.filter((id) => id !== productId),
+        })),
+    setCustomerId: (customerId) => set({ customerId }),
+    setBusinessId: (businessId) => set({ businessId }),
+    resetSale: () =>
+        set({
+            productIds: [],
+            customerId: "",
+            businessId: "",
+        }),
+}));
